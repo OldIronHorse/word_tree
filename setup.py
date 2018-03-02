d37be1cef32ae41e5a3cf741cbaae00aa68dfdf2
oldironhorse <s1m0n.r3dd1ng@gmail.com>
2018-03-02 14:28:01 +0000

    Version bump to 1.0

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ def readme():
     return f.read()
 
 setup(name='word_tree',
-      version='0.1',
+      version='1.0',
       description='Dictionary optimised for progressive lookup',
       long_description=readme(),
       keywords='dictionary word incremental lookup',
       url='http://github.com/OldIronHorse/word_tree',
-      download_url='https://github.com/oldironhorse/word_tree/archive/0.1.tar.gz',
+      download_url='https://github.com/oldironhorse/word_tree/archive/1.0.tar.gz',
       author='Simon Redding',
       author_email='s1m0n.r3dd1ng@gmail.com',
       license='GPL3',
